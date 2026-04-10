7c9bd3aa61fcd43ee6c204a2422efc2b1da13038
HatisG <1398197108@qq.com>
2026-04-10 17:42:58 +0800

    用户注册登录服务

---
+++ internal/user/service.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) Register(username, password string) (*User, error) {
+
+	_, err := s.repo.FindByUsername(username)
+	if err == nil {
+		return nil, errors.New("用户名已存在")
+	}
+
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, errors.New("密码加密失败")
+	}
+
+	user := &User{
+		Username: username,
+		Password: string(hashPassword),
+		Nickname: username,
+	}
+
+	if err := s.repo.Create(user); err != nil {
+		return nil, errors.New("用户创建失败")
+	}
+	return user, nil
+}
+
+func (s *Service) Login(username, password string) (*User, error) {
+
+	user, err := s.repo.FindByUsername(username)
+	if err != nil {
+		return nil, errors.New("用户名或密码错误")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("用户名或密码错误")
+	}
+
+	return user, nil
+}
